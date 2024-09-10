94a29339db22cb653f6b912f2eb658f23a15ac60
Olaoluwa Owoeye <Olaoluwaowoeye@gmail.com>
2024-09-10 19:27:04 +0100

    Testing the api

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ app.get("/", (req, res) => {
   res.send("Hello, world!!!!");
 });
 
-app.post("/api/products", (req, res) => {
+app.post("/api/products", async (req, res) => {
   try {
+    const product = await Product.create(req.body)
+    res.status(200).json(product);
     res.send(req.body);
   } catch (error) {
     res.status(500).json({ message: error.message });
