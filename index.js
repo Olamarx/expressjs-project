9f565407dcde88dbf0887ac39be5cb454fef3a47
Olaoluwa Owoeye <Olaoluwaowoeye@gmail.com>
2024-09-11 18:18:45 +0100

    Added getting and posting

--- index.js
+++ index.js
@@ -10,10 +10,20 @@ app.get("/", (req, res) => {
   res.send("Hello, world!!!!");
 });
 
-app.get("/api/products", async (req, res) => {
+app.get("/api/products/:id", async (req, res) => {
   try {
     const products = await Product.find({});
     res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
